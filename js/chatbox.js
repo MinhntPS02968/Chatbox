aefc0b0b69588ae470964cc46f6a98b87c3648e7
minhntps02968 <minhntps02968@fpt.edu.vn>
2020-03-16 21:19:36 +0700

    push

---
+++ js/chatbox.js
@@ -0,0 +1,19 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+function popup() {
+    var popup = document.getElementById("topup");
+    popup.style.display ="inline-block";
+    popup.style.transition = "height 0.5s";
+    popup.style.width ="70px";
+    popup.style.height ="300px";
+}
+function popout() {
+    var popup = document.getElementById("topup");
+    popup.style.width ="0";
+    popup.style.height ="0";
+}
